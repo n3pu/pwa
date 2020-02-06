776cf31732919a4b831a9b1d7df4197a4c405200
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2020-02-06 12:18:58 -0300

    alterações no sw

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,24 @@
 'use strict';
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'pontopoder.verdesmares.com.br-pwa-1.0.0';
 const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
   '/',
-  // '/index.html',
-  // '/scripts/app.js',
-  // '/scripts/install.js',
-  // '/scripts/luxon-1.11.4.js',
-  // '/styles/inline.css',
-  // '/images/add.svg',
-  // '/images/clear-day.svg',
-  // '/images/clear-night.svg',
-  // '/images/cloudy.svg',
-  // '/images/fog.svg',
-  // '/images/hail.svg',
-  // '/images/install.svg',
-  // '/images/partly-cloudy-day.svg',
-  // '/images/partly-cloudy-night.svg',
-  // '/images/rain.svg',
-  // '/images/refresh.svg',
-  // '/images/sleet.svg',
-  // '/images/snow.svg',
-  // '/images/thunderstorm.svg',
-  // '/images/tornado.svg',
-  // '/images/wind.svg',
+  '/service-worker.js',
+  '/manifest.json',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-256x256.png',
+  '/images/icons/icon-512x512.png',
 ];
 
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  // Precache static resources here.
+  // CODELAB: Precache static resources here.
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
@@ -43,7 +30,7 @@ self.addEventListener('install', (evt) => {
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-  // Remove previous cached data from disk.
+  // CODELAB: Remove previous cached data from disk.
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
@@ -60,7 +47,7 @@ self.addEventListener('activate', (evt) => {
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
-  // Add fetch event handler here.
+  // CODELAB: Add fetch event handler here.
   if (evt.request.url.includes('/forecast/')) {
     console.log('[Service Worker] Fetch (data)', evt.request.url);
     evt.respondWith(
